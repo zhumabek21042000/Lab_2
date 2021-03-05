ec18732be3aa1205b4c8d80e0440ebfdc3be5805
zhumabek21042000 <58175222+zhumabek21042000@users.noreply.github.com>
2021-03-06 04:56:06 +0500

    Add files via upload

---
+++ AspNetCore-StarChartAPI/StarChart/Models/CelestialObjectDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarChart.Models
+{
+    public class CelestialObjectDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? OrbitedObjectId { get; set; }
+        [NotMapped]
+        public List<CelestialObject> Satellites { get; set; }
+        public TimeSpan OrbitalPeriod { get; set; }
+    }
+}
